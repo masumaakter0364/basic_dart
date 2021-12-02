80a0bb7deab99b74b20f28c858e6478efaac7d42
Rima <masum aakter0364@gmail.com>
2021-12-03 00:30:22 +0600

    question6 done

--- bin/firstproject.dart
+++ bin/firstproject.dart
@@ -1,4 +1,5 @@
 
+import'dart:math';
 
 void main() {
 
@@ -7,6 +8,11 @@ question2();
 question3();
 question4();
 question5();
+question6();
+question7();
+question8();
+question9();
+question10();
 }
 
 //01. Write a Dart program to print 'Hello' on screen and then print your name on a separate line.
@@ -80,3 +86,61 @@ void question5(){
 //06. Write a Dart program to print the area (A=πr2) and
 // perimeter of a circle (C=2πr) here π=3.1416
 
+void question6(){
+  int r=5;
+  double area=(3.1416*pow(r,2));
+  double perimeter=(2*3.1416*r);
+  print('$area,  $perimeter');
+}
+
+//07. Make a reusable method  and pass two values inside the method,
+// calculate division of two values, get the return value and store it
+// on a new variable inside the main method and print the new variable.
+void question7(){
+
+}
+void calculation(){
+  
+}
+
+
+
+//08. Write a method to display your id, your name, your email, your phone number in single line.
+void question8(){
+print('94,  Masuma,  masumaakter0364@gmail.com,  01640910364');
+}
+
+
+//09. Write two variable inside the main method assign the given values 83, 11
+// now create a new method named is myNewMethod pass given values inside the new method calculate
+// the multiplication, now create another function named is myNewMethod2 to display
+// the result of multiplication, pass the result of multiplication inside the
+// myNewMethod2 and display it.
+void question9(){
+
+}
+
+
+//10. Write a Dart program to print the result of the following operations.
+// Test Data:
+// a. -5 + 8 * 6
+// b. (55+9) % 9
+// c. 20 + -3*5 / 8
+// d. 5 + 15 / 3 * 2 - 8 % 3
+// Expected Output :
+// 43
+// 1
+// 19
+// 13
+
+void question10(){
+  int a=((-5)+8*6);
+  int b=((55+9)%9);
+  double c= (20+(-3)*5/8);
+  double d=5+((15/3)*2)-(8%3);
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+
+}
