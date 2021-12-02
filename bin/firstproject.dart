f49b7a4cd3cba598428847101c36082420ef9dda
Rima <masum aakter0364@gmail.com>
2021-12-02 22:01:10 +0600

    question1 done

--- bin/firstproject.dart
+++ bin/firstproject.dart
@@ -1,3 +1,12 @@
 void main() {
-  print('Hello world!');
+
+question1();
 }
+
+//01. Write a Dart program to print 'Hello' on screen and then print your name on a separate line.
+
+void question1(){
+  print('Hello \nMasuma Akter Rima');
+}
+
+
