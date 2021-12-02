a18f258bc3cce07fe256c249be840de17791be08
Rima <masum aakter0364@gmail.com>
2021-12-02 22:08:44 +0600

    question2 done

--- bin/firstproject.dart
+++ bin/firstproject.dart
@@ -1,6 +1,7 @@
 void main() {
 
 question1();
+question2();
 }
 
 //01. Write a Dart program to print 'Hello' on screen and then print your name on a separate line.
@@ -8,5 +9,11 @@ question1();
 void question1(){
   print('Hello \nMasuma Akter Rima');
 }
-
+//02. Write a Dart program to print the sum of two numbers.
+void question2(){
+  int a=2;
+  double b= 2.5;
+  double sum=a+b;
+  print(sum);
+}
 
