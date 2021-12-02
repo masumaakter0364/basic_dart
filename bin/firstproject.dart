b91846139e5841c4e326bc76122a9fa9b8c49c48
Rima <masum aakter0364@gmail.com>
2021-12-02 22:52:26 +0600

    question3 done

--- bin/firstproject.dart
+++ bin/firstproject.dart
@@ -1,7 +1,10 @@
+
+
 void main() {
 
 question1();
 question2();
+question3();
 }
 
 //01. Write a Dart program to print 'Hello' on screen and then print your name on a separate line.
@@ -16,4 +19,30 @@ void question2(){
   double sum=a+b;
   print(sum);
 }
+// 03. Create a method and declare some Dart variable inside the method and store the given    values on your variable, don't use var keyword to declare the variable.
+//    a) 12
+//    b) '12'
+//    c) 'Your name'
+//    d) '23.4'
+//    e) ['Your name', 'Your Father name', 'Sister name']
+//    f) [1,2,3,4]
+//    g) {'id':'110', 'name':'your_name'}
+void question3() {
+  int a = 12;
+  String b='12';
+  String c= 'Masuma Akter';
+  String d= '23.4';
+  List x1= ['Masuma','Mizanur Rahman', 'Mahfuza'];
+  List x2= [1,2,3,4];
+  Map y=({'id':'94', 'name':'Masuma Akter'});
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(x1);
+  print(x2);
+  print(y);
+}
+//04. Write a Dart program that takes two numbers  and displays the product of two numbers.
+
 
